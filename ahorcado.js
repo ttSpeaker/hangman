0eb8aeb925eb1c6e55eb03499b2a9e2045a510f6
ttSpeaker <js.serruya@gmail.com>
2018-08-23 19:17:37 -0300

    primer commit del branch

--- ahorcado.js
+++ ahorcado.js
@@ -1,40 +1,22 @@
 var palabraClave = prompt("Ingrese palabra a adivinar: ");
-var arrayClave= palabraClave.split("");
-var adivinando = [];
+var adivinando = "";
 var vidas=5;
 var gano = false;
-for (var i = 0; i<arrayClave.length; i++) {
-  adivinando[i]=("_ ");
-}
-console.log(palabraClave[2])
+
 function adivinarLetra (){
   var adivino=false;
   var letraNueva = prompt("Adivine una letra: ");
   for(let j=0; j<palabraClave.length; j++){
     if (letraNueva==palabraClave[j]) {
       adivino=true;
-      adivinando[j]=letraNueva;
-      alert(adivinando);
-      console.log("adivino");
+      alert("Adivinaste la letra: ''" + letraNueva + "'' en el indice: " + j);
+      return letraNueva;
       }
     }
-    if (!adivino) {
-      vidas--;
-      alert("Letra equivocada. Vidas restantes: "+vidas);
-    }
+    vidas--;
+    alert("Letra equivocada. Vidas restantes: "+vidas);
   }
-
-function chequearSiGano (){
-  console.log(arrayClave);
-  console.log(adivinando);
-  if (arrayClave == adivinando){
-    alert("Ganaste!! La palabra clave era: "+ palabraClave);
-    return true;
-  } else {
-    return false;
-  }
-}
 for (vidas;vidas>=0;vidas--){
   adivinarLetra();
-  chequearSiGano();
+  // chequearSiGano();
 }
